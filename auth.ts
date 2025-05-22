42772cb43c734a2b27cfcf749cad5ac292db3b01
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-05-22 16:10:49 -0400

    add: custom jwt to reflect user role

--- auth.ts
+++ auth.ts
@@ -55,12 +55,33 @@ export const config = {
         async session({ session, user, trigger, token }: any) { //<< any temporary will update
             // set user from token sub property
             session.user.id = token.sub
+            session.user.role = token.role
+            session.user.name = token.name
+            //console.log(token)
             // update name is session and db
             if(trigger === 'update') {
                 session.user.name = user.name
             }
 
             return session
+        },
+        async jwt({token, user, trigger, session}: any) {
+            // Assign user fields to token
+            if(user) {
+                token.role = user.role;
+
+                // if user has no name, use email
+                if(user.name === 'NO_NAME') {
+                    token.name = user.email!.split('@')[0]
+
+                    // Update database to reflect token name
+                    await prisma.user.update({
+                        where: {id: user.id},
+                        data: {name: token.name}
+                    })
+                }
+            }
+            return token
         },
     }
 
