9757cfbc76ee7dba3ea7035564e9b4a64e9a4a9f
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-05-24 16:46:48 -0400

    add: sessionCartId for guests and users

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
-import {PrismaAdapter} from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/db/prisma"
 import  CredentialsProvider from "next-auth/providers/credentials"
 import { compareSync } from "bcrypt-ts-edge"
 import type { NextAuthConfig } from "next-auth"
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+
 
 // add google social log in future update
 export const config = {
@@ -83,6 +86,31 @@ export const config = {
             }
             return token
         },
+        authorized({request, auth}: any) {
+            // Check session cart cookie 
+            if(!request.cookies.get('sessionCartId')) {
+               // Generate new session cart id cookie session start
+               const sessionCartId = crypto.randomUUID()
+               
+               // Clone request headers
+               const newRequestHeaders = new Headers(request.headers)
+
+               // Create new response and add new headers
+                const response = NextResponse.next({
+                    request: {
+                        headers: newRequestHeaders
+                    }
+                })
+
+                // Set new sessionCartId in response cookies
+                response.cookies.set('sessionCartId', sessionCartId)
+
+                return response
+
+            } else {
+                return true
+            }
+        }
     }
 
 } satisfies NextAuthConfig;
