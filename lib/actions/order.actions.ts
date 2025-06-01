309f94f23bd9ebdeacf95f4d80868f38d7ce46bb
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-01 14:29:07 -0400

    add: action to get order by id, component page to display order details and place order

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { isRedirectError } from "next/dist/client/components/redirect-error"
-import { formatError } from "../utils"
+import { convertPrismaObjToJSObj, formatError } from "../utils"
 import { auth } from "@/auth"
 import { getMyCart } from "./cart.actions"
 import { getUserById } from "./user.actions"
@@ -99,4 +99,20 @@ export async function createOrder() {
             message: formatError(error)
         }
     }
+}
+
+// Get order by id
+
+export async function getOrderById(orderId: string) {
+    const data = await prisma.order.findFirst({
+        where: {
+            id: orderId
+        },
+        include: {
+            orderitems: true,//<< items included when we get the order
+            user: {select: { name: true, email: true } }// include user name and email
+        },
+    })
+
+    return convertPrismaObjToJSObj(data)
 }
