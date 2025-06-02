70e485c7e9f08e779b16fce08e5b6f1b57115f5d
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-02 19:02:15 -0400

    fix: cart would empty when guest sign in

--- auth.ts
+++ auth.ts
@@ -72,7 +72,7 @@ export const config = {
             // Assign user fields to token
             if(user) {
                 token.id = user.id
-
+                token.role = user.role
                 // if user has no name, use email
                 if(user.name === 'NO_NAME') {
                     token.name = user.email!.split('@')[0]
@@ -83,7 +83,29 @@ export const config = {
                         data: {name: token.name}
                     })
                 }
-            
+                if(trigger === 'signIn' || trigger === 'signUp') {
+                   const cookiesObject = await cookies()
+                   const sessionCartId = cookiesObject.get('sessionCartId')?.value
+
+                   if(sessionCartId) {
+                    const sessionCart = await prisma.cart.findFirst({
+                        where: { sessionCartId }
+                    });
+                    if(sessionCart) {
+                        // Delete current user cart
+                        await prisma.cart.deleteMany({
+                            where: {userId: user.id},
+                        });
+
+                        // Assign new cart
+                        await prisma.cart.update({
+                            where: {id: sessionCart.id},
+                            data: {userId: user.id}
+                        })
+                    }
+
+                   }
+                }
             }
             return token
         },
