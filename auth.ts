ae686951a44a76b9a2ac140211a12fda7e24366f
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-05 13:39:22 -0400

    add: update token name with session name

--- auth.ts
+++ auth.ts
@@ -107,6 +107,11 @@ export const config = {
                    }
                 }
             }
+            // Handle session updates
+            if(session?.user.name && trigger === 'update') {
+                token.name = session.user.name
+            }
+
             return token
         },
         authorized({request, auth}: any) {
