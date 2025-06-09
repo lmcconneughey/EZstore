e319e21164a324660e9fc573b8d6b7c9ab453d6f
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-09 12:57:26 -0400

    add: action to get all products

--- lib/actions/product-actions.ts
+++ lib/actions/product-actions.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from "../../db/prisma";
 import { convertPrismaObjToJSObj } from "../utils";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 // Get latest products
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
@@ -18,5 +18,31 @@ export async function getProductBySlug(slug: string) {
     return await prisma.product.findFirst({
         where: {slug: slug}
     })
+}
+
+// Get all products
+
+export async function getAllProducts({
+    query,
+    limit = PAGE_SIZE,
+    page,
+    category
+}: {
+    query: string;
+    limit?: number;
+    page: number;
+    category?: string
+}) {
+    const data = await prisma.product.findMany({
+        skip: (page -1) * limit,
+        take: limit  
+    });
+
+    const dataCount = await prisma.product.count();
+
+    return {
+        data,
+        totalPages: Math.ceil(dataCount / limit)
+    }
 }
 
