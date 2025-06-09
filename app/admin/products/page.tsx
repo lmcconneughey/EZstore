d6748248225f0336a8f1d09b194fcd37e8b219f1
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-09 14:08:30 -0400

    add: delete product button and delete dialog warning

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import { getAllProducts } from "@/lib/actions/product-actions";
+import { getAllProducts, deleteProduct } from "@/lib/actions/product-actions";
 import { formatCurrency, formatId } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Pagination from "@/components/shared/pagination";
+import DeleteDialog from "@/components/shared/delete-dialog";
 
 const AdminProductsPage = async (props: {
     searchParams: Promise<{
@@ -24,9 +25,6 @@ const AdminProductsPage = async (props: {
         page,
         category
     })
-
-    //console.log(products);
-    
 
     return (
         <div className="space-y-2">
@@ -69,7 +67,10 @@ const AdminProductsPage = async (props: {
                                         Edit
                                     </Link>
                                 </Button>
-                                {/* DELETE */}
+                                <DeleteDialog 
+                                    id={product.id} 
+                                    action={deleteProduct}
+                                />
                             </TableCell>
                         </TableRow>
                     )) }
