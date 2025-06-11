87d03bde29e85842c2c9d04606b38068517797b3
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-11 11:38:03 -0400

    add: default values for create products form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -32,3 +32,17 @@ export const DEFFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || 'Pa
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 10;
 
+export const productDefaultValues = {
+    name: '',
+    slug: '',
+    category: '',
+    images: [],
+    brand: '',
+    description: '',
+    price: '0',
+    stock: 0,
+    rating: '0',
+    numReviews: '0',
+    isFeatured: false,
+    banner: null,
+}
