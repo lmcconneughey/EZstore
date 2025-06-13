2603ddb0d2139608e97f275072e977aa3d50d636
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-13 14:07:23 -0400

    style: align items in header

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -5,8 +5,8 @@ import Menu from "./menu";
 import CategoryDrawer from "./category-drawer";
 
 const Header = () => {
-    return  <header className="w-full border-b">
-        <div className="wrapper flex-betweeen">
+    return  <header className="flex flex-col w-full border-b">
+        <div className="flex flex-between h-16 px-4 wrapper">
             <div className="flex-start">
                 <CategoryDrawer />
                 <Link href='/' className="flex-start ml-2">
