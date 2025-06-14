d2e8ecf4cbb4b04930a5f18001e9178dab971790
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-14 17:41:27 -0400

    add: price and category filter

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,5 +1,29 @@
 import ProductCard from "@/components/shared/product/product-card";
-import { getAllProducts } from "@/lib/actions/product-actions";
+import { getAllProducts, getAllCategories } from "@/lib/actions/product-actions";
+import Link from "next/link";
+
+const prices = [
+    {
+        name: '$1 to $50',
+        value: '1-50'
+    },
+    {
+        name: '$51 to $100',
+        value: '51-100'
+    },
+    {
+        name: '$101 to $200',
+        value: '101-200'
+    },
+    {
+        name: '$201 to $500',
+        value: '201-500'
+    },
+    {
+        name: '$501 to $1000',
+        value: '501-1000'
+    },
+];
 
 const SearchPage = async (props: {
     searchParams: Promise<{
@@ -19,6 +43,31 @@ const SearchPage = async (props: {
         sort = 'newest',
         page = '1',
     } = await props.searchParams;
+
+    // Filter Url
+    const getFilterUrl = ({
+        c,// category
+        s,// sort
+        p,// price
+        r,// rating
+        pg,// page
+    }: {
+        c?: string;
+        p?: string;
+        s?: string;
+        r?: string;
+        pg?: string;
+    }) => {
+        const params = {q, category, price, rating, sort, page};
+
+        if(c) params.category = c;
+        if(p) params.price = p;
+        if(s) params.sort = s;
+        if(r) params.rating = r;
+        if(pg) params.page = pg;
+
+        return `/search?${new URLSearchParams(params).toString()}`
+    };
     
     const products = await getAllProducts({
         query: q,
@@ -29,10 +78,63 @@ const SearchPage = async (props: {
         page: Number(page)
     })
 
+    const categories = await getAllCategories()
+
     return ( 
         <div className="grid md:grid-cols-5 md:gap-5">
             <div className="filter-links">
-                {/* FILTERS */}
+                {/* Category links */}
+                <div className="text-xl mb-2 mt-3">
+                    Department
+                </div>
+                <div>
+                    <ul className="space-y-1">
+                        <li>
+                            <Link
+                                className={`${(category === 'all' || category === '') && 'font-bold'}`} 
+                                href={getFilterUrl({c: 'all'})}
+                            >   
+                                Any
+                            </Link>
+                        </li>
+                        { categories.map ((x) => (
+                            <li key={x.category}>
+                                <Link 
+                                    className={`${category === x.category && 'font-bold'}`}
+                                    href={getFilterUrl({ c: x.category })}
+                                >
+                                    {x.category}
+                                </Link>
+                            </li>
+                        )) }
+                    </ul>
+                </div>
+                {/* Price links */}
+                <div className="text-xl mb-2 mt-8">
+                    Price
+                </div>
+                <div>
+                    <ul className="space-y-1">
+                        <li>
+                            <Link
+                                className={`${price === 'all' && 'font-bold'}`} 
+                                href={getFilterUrl({p: 'all'})}
+                            >   
+                                Any
+                            </Link>
+                        </li>
+                        { prices.map ((p) => (
+                            <li key={p.value}>
+                                <Link 
+                                    className={`${price === p.value && 'font-bold'}`}
+                                    href={getFilterUrl({ p: p.value })}
+                                >
+                                    {p.name}
+                                </Link>
+                            </li>
+                        )) }
+                    </ul>
+                </div>
             </div> 
             <div className="md:col-span-4 space-y-4">
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
