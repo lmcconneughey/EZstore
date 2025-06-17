e1a39d441fa71fdaae465f992640793669dbf3e4
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-17 14:26:13 -0400

    add: review types, validators and default values

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -47,4 +47,10 @@ export const productDefaultValues = {
     banner: null,
 }
 
-export const USER_ROLES = process.env.USER_ROLES ? process.env.USER_ROLES.split(', ') : ['admin', 'user']
+export const USER_ROLES = process.env.USER_ROLES ? process.env.USER_ROLES.split(', ') : ['admin', 'user'];
+
+export const reviewFormDefaultValues = {
+    title: '',
+    comment: '',
+    rating: 0,
+}
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,14 @@
 import {z} from "zod"
-import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderItemSchema, insertOrderSchema, paymentResultSchema } from "@/lib/validators"
+import { 
+    insertProductSchema, 
+    insertCartSchema, 
+    cartItemSchema, 
+    shippingAddressSchema, 
+    insertOrderItemSchema, 
+    insertOrderSchema, 
+    paymentResultSchema,
+    insertReviewSchema 
+} from "@/lib/validators"
 
 export type Product = z.infer<typeof insertProductSchema> &{
     id: string;//<< not in zod schema
@@ -27,3 +36,9 @@ export type Order = z.infer<typeof insertOrderSchema> & {
 }
 
 export type PaymentResult = z.infer<typeof paymentResultSchema>
+
+export type Review = z.infer<typeof insertReviewSchema> & {
+    id: string;
+    createdAt: Date;
+    user?: {name: string};
+}
