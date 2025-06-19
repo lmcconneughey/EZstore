0a2bf14019af9e50423c71f726f78a3464d8efbb
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-19 15:14:31 -0400

    fix: added numReviews to product type

--- types/index.ts
+++ types/index.ts
@@ -13,6 +13,7 @@ import {
 export type Product = z.infer<typeof insertProductSchema> &{
     id: string;//<< not in zod schema
     rating: string;
+    numReviews: number;
     createdAt: Date;
 }
 
