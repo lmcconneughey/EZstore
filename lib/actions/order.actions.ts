eed7854fd9da6796aec020d3ef6d4e94f1b7ad11
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-24 14:09:39 -0400

    add: action to email purchase receipt when order is placed

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,11 +7,12 @@ import { getMyCart } from "./cart.actions"
 import { getUserById } from "./user.actions"
 import { insertOrderSchema } from "../validators"
 import { prisma } from "@/db/prisma"
-import { CartItem, PaymentResult } from "@/types"
+import { CartItem, PaymentResult, ShippingAddress } from "@/types"
 import { paypal } from "../paypal"
 import { revalidatePath } from "next/cache"// order page revalidate and refresh once paid
 import { PAGE_SIZE } from "../constants"
 import { Prisma } from "../generated/prisma"
+import { sendPurchaseReceipt } from "@/email"
 
 // Create  order and create order items
 export async function createOrder() {
@@ -266,7 +267,15 @@ export async function updateOrderToPaid({
         }
     });
 
-    if(!updatedOrder) throw new Error('Order not found')
+    if(!updatedOrder) throw new Error('Order not found');
+
+    sendPurchaseReceipt({
+        order: {
+            ...updatedOrder,
+            shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+            paymentResult: updatedOrder.paymentResult as PaymentResult,
+        },
+    });
 
 }
 
