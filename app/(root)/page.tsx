9ced35db84e283516c21c0835189fbe7bb6a95c5
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-06-24 14:37:55 -0400

    add: icon boxes at homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product-actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
 
 export const metadata = {
   title: 'home'
@@ -25,6 +26,7 @@ const Homepage = async () => {
           limit={4}
       />
       <ViewAllProductsButton />
+      <IconBoxes />
   </div> );
 }
  
